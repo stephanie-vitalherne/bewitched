4215d3ad5b18d6857c04b4b62acf2208183928de
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-11-13 00:24:07 -0500

    login done

--- screens/Login/index.js
+++ screens/Login/index.js
@@ -1,13 +1,45 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { View, Image, Pressable, Text } from "react-native";
 import useAuth from "../../hooks/useAuth";
+import { useNavigation } from "@react-navigation/core";
+import { LinearGradient } from "expo-linear-gradient";
+import tw from "tailwind-rn";
 
 const Login = () => {
   const { signInWithGoogle } = useAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text></Text>
-      <Button title="Login" onPress={signInWithGoogle} />
+    <View style={tw("flex-1")}>
+      <LinearGradient
+        style={tw("absolute left-0 right-0 top-0 h-full")}
+        end={{ x: 0.65, y: 1.2 }}
+        locations={[0.01, 0.28, 0.87]}
+        colors={["rgba(255,151,0,0.8)", "rgba(49,4,103,1)", "rgba(28,6,54,1)"]}
+      />
+      <View style={tw("w-full h-full justify-center items-center")}>
+        <Image
+          source={require("../../assets/magic-ball.png")}
+          style={tw("w-72 h-72")}
+        />
+      </View>
+      <Pressable
+        style={[
+          tw("absolute bottom-40 w-52 bg-gray-700 p-4 rounded-2xl"),
+          { marginHorizontal: "25%" },
+        ]}
+        onPress={signInWithGoogle}
+      >
+        <Text style={tw("text-center text-white font-semibold")}>
+          Sign In & Get to Swiping
+        </Text>
+      </Pressable>
     </View>
   );
 };
