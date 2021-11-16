db1d706d617f00d0cdd908a551bcc5f1d15a67ae
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-11-15 22:57:20 -0500

    reseting gitignore

--- components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as SwipeCard } from "./SwipeCard";
-export { default as NoProfile } from "./NoProfile";
-export { default as Buttons } from "./Buttons";
-export { default as Header } from "./Header";
